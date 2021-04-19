00489f1b86e13c5a541ea9b63a201ddcc4899b02
bhavikapatel1996 <bhavika.patel8596@gmail.com>
2021-04-19 11:55:02 -0400

    changes- front end

--- mycapstone/home/models.py
+++ mycapstone/home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+# class UploadFileForm(forms.Form):
+#     title = forms.CharField(max_length=50)
+#     file = forms.FileField()
--- mycapstone/home/views.py
+++ mycapstone/home/views.py
@@ -63,3 +63,27 @@ def upload(request):
     }
 
     return render(request, "upload.html", context)
+
+###################################################################
+
+# from django.http import HttpResponseRedirect
+# from django.shortcuts import render
+# from .forms import UploadFileForm
+
+# # Imaginary function to handle an uploaded file.
+# from somewhere import handle_uploaded_file
+
+# def upload_file(request):
+#     if request.method == 'POST':
+#         form = UploadFileForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             handle_uploaded_file(request.FILES['file'])
+#             return HttpResponseRedirect('/success/url/')
+#     else:
+#         form = UploadFileForm()
+#     return render(request, 'file.html', {'form': form})
+
+# def upload(request):
+#     context = {
+#         'Form': form,
+#     }
